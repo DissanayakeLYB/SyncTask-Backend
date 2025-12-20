4fbbe26e20cbcbe88e4cda23d60011d2d03f7bc6
DissanayakeLYB <lasithdissanayake99@gmail.com>
2025-12-20 18:30:54 +0530

    update: remove user endpoint and add /leaves, /tasks endpoints

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
-import bcrypt from "bcrypt";
 import cors from "cors";
+// import mongoose from "mongoose";
+
+// mongoose.connect("mongodb://localhost/tasks");
 
 const server = express();
 const PORT = 3000;
@@ -8,25 +10,38 @@ const PORT = 3000;
 server.use(cors());
 server.use(express.json());
 
-const users = [];
+// { taskDescription: "Random Task to complete", status: "todo" responsible: ["lasith", "ashen"]}
+const tasks = [];
 
-server.get("/users", (req, res) => {
-  res.json(users);
+// { name: "lasith", leave_dates: [{ year: 2025, month: 12, day: 23}]}
+const leaves = [];
+
+server.get("/tasks", (req, res) => {
+  return res.json(tasks);
 });
 
-server.post("/users", async (req, res) => {
+server.post("/tasks", (req, res) => {
   try {
-    const salt = await bcrypt.genSalt(10);
-    const name = req.body.name;
-    const password = req.body.password;
-
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const user = { name: name, password: hashedPassword };
-    users.push(user);
-    res.status(201).send("Succesfully created");
+    const task = req.body;
+    tasks.push(task);
+    res.status(201).send("successfully added the task");
   } catch {
-    res.status(500).send("Failed from server");
+    res.status(500).send("Failed to add the task");
+  }
+});
+
+server.get("/leaves", (req, res) => {
+  res.json(leaves);
+});
+
+server.post("/leaves", (req, res) => {
+  try {
+    const leave = req.body;
+
+    leaves.push(leave);
+    res.status(201).send("Successfully added the leave");
+  } catch {
+    res.status(500).send("Failed to add the leave.");
   }
 });
 
